450f8701b351b9786377ba01f49782c77fe90e75
GUSTAVO BRANDAO NERSISSIAN <gustavo.bnersissian@senacsp.edu.br>
2022-08-16 21:18:56 -0300

    3

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from pickletools import optimize
 from PIL import Image
+
+# quality = 0 a 1
 
 def img_converter(input_file, output_file, format):
     image = Image.open(input_file)
-    image.save(output_file, format = format)
+    image.save(output_file, format = format, optimize = True, quality = 1)
+
+def to_thumbnail(input_file):
+    image = Image.open(input_file)
+    image.thumbnail((75,75))
+    image.save("thumbnail.jpg")
 
 def img_format(input_file):
     imagem = Image.open(input_file)
@@ -13,3 +21,5 @@ if __name__ == "__main__":
     img_format("img.jpg")
     img_format("pizza.png")
     img_format("pizza.teste")
+    to_thumbnail("img.jpg")
+
